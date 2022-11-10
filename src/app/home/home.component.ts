272ef4e03e3f08d6b1325d8575fa25a629096ad0
natlee77 <70273134+natlee77@users.noreply.github.com>
2022-11-10 12:34:34 +0100

    added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -23,21 +23,18 @@ bases=['UAH','USD','EUR'];
   result: any=[];
   amount: any=[];
   price: any=[];
-  public selectedbase=".";
-  public selectedcont2=".";
+  public selectedbase="...";
+  public selectedcont2="...";
     changebase(event: any){
          this.base = this.selectedbase;
-            // console.log('base :', this.base)
     }
 
   tocounty(event:any){
     this.cont2 = this.selectedcont2 ;
-    //  console.log('cont2 : ',this.cont2)
   }
   amounts(c: string ){
     this.amount = c;
     this.result=0;
-    // console.log('amount: ',this.amount)
   }
   constructor(private currency: CurrencyapidataService) {
   }
@@ -49,9 +46,7 @@ bases=['UAH','USD','EUR'];
   }
   convert(){
     this.currency.getcurrencyapidata(this.base).subscribe(data =>{
-      // console.log(data)
        this.currjson=JSON.stringify(data);
-        //  console.log(this.currjson)
        this.currjson= JSON.parse(this.currjson)
          console.log(this.currjson )
 
@@ -59,12 +54,10 @@ bases=['UAH','USD','EUR'];
          if (this.cont2=='EUR'){
           this.price  = this.currjson[0].buy
           this.result =(this.amount * this.price / this.currjson[1].sale).toFixed(2)
-          console.log(this.result)
         }
         if (this.cont2=='UAH' ){
           this.price = this.currjson[0].buy
           this.result =(this.price * this.amount).toFixed(2)
-          console.log(this.result)
         }
 
       }
@@ -73,12 +66,10 @@ bases=['UAH','USD','EUR'];
         if (this.cont2=='USD'){
          this.price  = this.currjson[1].buy
          this.result =(this.amount * this.price / this.currjson[0].sale).toFixed(2)
-         console.log(this.result)
        }
        if (this.cont2=='UAH' ){
          this.price = this.currjson[1].buy
          this.result = (this.price * this.amount).toFixed(2)
-         console.log(this.result)
        }
       }
 
@@ -86,12 +77,10 @@ bases=['UAH','USD','EUR'];
           if (this.cont2=='EUR'){
             this.price  = this.currjson[1].sale
             this.result =(this.amount / this.price).toFixed(2)
-            console.log(this.result)
           }
           if (this.cont2=='USD' ){
             this.price = this.currjson[0].sale
             this.result =   ( this.amount /this.price).toFixed(2)
-            console.log(this.result)
           }
         }
        if (this.base==this.cont2){
